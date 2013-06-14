066fdb95ba3005cdd67e72dc958cf303fa22ccd2
Francois Lebel <francoislebel@gmail.com>
2013-06-13 23:41:37 -0400

    Added .gitconfig file.

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone git://github.com/creationix/nvm.git ~/nvm
 
 ln -s ${CURRENT}/.Xdefaults ~/.Xdefaults
+ln -s ${CURRENT}/.gitconfig ~/.gitconfig
 ln -s ${CURRENT}/.inputrc ~/.inputrc
 ln -s ${CURRENT}/.zshrc ~/.zshrc
 
