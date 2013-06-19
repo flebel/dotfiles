5f22dd6cb0684222b381f1b9097f2a8e0d0a81ca
Francois Lebel <francoislebel@gmail.com>
2013-06-19 19:49:35 -0400

    Moved all dotfiles into an assets directory.

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-CURRENT=`pwd`
+ASSETS=`pwd`/assets
 
 chsh -s /bin/zsh
 git clone git://github.com/flebel/oh-my-zsh.git ~/.oh-my-zhs
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone git://github.com/creationix/nvm.git ~/nvm
 
-ln -s ${CURRENT}/.Xdefaults ~/.Xdefaults
-ln -s ${CURRENT}/.gitconfig ~/.gitconfig
-ln -s ${CURRENT}/.hgrc ~/.hgrc
-ln -s ${CURRENT}/.inputrc ~/.inputrc
-ln -s ${CURRENT}/.zshrc ~/.zshrc
+ln -s ${ASSETS}/.Xdefaults ~/.Xdefaults
+ln -s ${ASSETS}/.gitconfig ~/.gitconfig
+ln -s ${ASSETS}/.hgrc ~/.hgrc
+ln -s ${ASSETS}/.inputrc ~/.inputrc
+ln -s ${ASSETS}/.zshrc ~/.zshrc
 
