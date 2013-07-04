fded2514ac62f797bb7504ae134add92b9d899dc
Francois Lebel <francoislebel@gmail.com>
2013-07-04 01:14:27 -0400

    Replaced the static list of files by a loop over the directory's content.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 ASSETS=`pwd`/assets
+ASSETS_FILES=`find $ASSETS -maxdepth 1 -type f`
 SCRIPTS=`pwd`/scripts
 
 chsh -s /bin/zsh
@@ -8,12 +9,12 @@ git clone git://github.com/flebel/oh-my-zsh.git ~/.oh-my-zhs
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone git://github.com/creationix/nvm.git ~/nvm
 
-ln -s ${ASSETS}/.Xdefaults ~/.Xdefaults
-ln -s ${ASSETS}/.gitconfig ~/.gitconfig
-ln -s ${ASSETS}/.grepoptions ~/.grepoptions
-ln -s ${ASSETS}/.hgrc ~/.hgrc
-ln -s ${ASSETS}/.inputrc ~/.inputrc
-ln -s ${ASSETS}/.zshrc ~/.zshrc
+mkdir ~/dotfiles_backup
+for f in $ASSETS_FILES; do
+  filename="${f##*/}"
+  mv ~/$filename ~/dotfiles_backup/
+  ln -s ${ASSETS}/$filename ~/$filename
+done
 
 mkdir ~/scripts
 ln -s ${SCRIPTS}/lock.sh ~/scripts/lock.sh
