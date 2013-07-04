f617f5b03068bce4ac3670502fda31f07ade4be4
Francois Lebel <francoislebel@gmail.com>
2013-07-04 01:16:14 -0400

    Silented the mkdirs.

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ git clone git://github.com/flebel/oh-my-zsh.git ~/.oh-my-zhs
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone git://github.com/creationix/nvm.git ~/nvm
 
-mkdir ~/dotfiles_backup
+mkdir ~/dotfiles_backup > /dev/null 2>&1
 for f in $ASSETS_FILES; do
   filename="${f##*/}"
   mv ~/$filename ~/dotfiles_backup/
   ln -s ${ASSETS}/$filename ~/$filename
 done
 
-mkdir ~/scripts
+mkdir ~/scripts > /dev/null 2>&1
 ln -s ${SCRIPTS}/lock.sh ~/scripts/lock.sh
 ln -s ${SCRIPTS}/set_trackball_acceleration.sh ~/scripts/set_trackball_acceleration.sh
 
