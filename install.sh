89687e74457c8020b490876bf6cd2ba7df23440b
Francois Lebel <francoislebel@gmail.com>
2013-07-06 00:50:54 -0400

    Replaced the static list of files by a loop over the directory's content.

--- install.sh
+++ install.sh
@@ -3,20 +3,25 @@
 ASSETS=`pwd`/assets
 ASSETS_FILES=`find $ASSETS -maxdepth 1 -type f`
 SCRIPTS=`pwd`/scripts
+SCRIPTS_FILES=`find $SCRIPTS -maxdepth 1 -type f`
 
 chsh -s /bin/zsh
 git clone git://github.com/flebel/oh-my-zsh.git ~/.oh-my-zhs
 git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone git://github.com/creationix/nvm.git ~/nvm
 
-mkdir ~/dotfiles_backup > /dev/null 2>&1
+mkdir -p ~/dotfiles_backup/assets > /dev/null 2>&1
 for f in $ASSETS_FILES; do
   filename="${f##*/}"
-  mv ~/$filename ~/dotfiles_backup/
+  mv ~/$filename ~/dotfiles_backup/assets/
   ln -s ${ASSETS}/$filename ~/$filename
 done
 
 mkdir ~/scripts > /dev/null 2>&1
-ln -s ${SCRIPTS}/lock.sh ~/scripts/lock.sh
-ln -s ${SCRIPTS}/set_trackball_acceleration.sh ~/scripts/set_trackball_acceleration.sh
+mkdir -p ~/dotfiles_backup/scripts > /dev/null 2>&1
+for f in $SCRIPTS_FILES; do
+  filename="${f##*/}"
+  mv ~/$filename ~/dotfiles_backup/scripts/
+  ln -s ${SCRIPTS}/$filename ~/scripts/$filename
+done
 
