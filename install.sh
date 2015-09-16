f7fdcef2fb8863beb82b15748572747c9dc2b098
Francois Lebel <francoislebel@gmail.com>
2015-09-16 11:06:40 -0700

    Run qtile-related stuff only on GNU/Linux.

--- install.sh
+++ install.sh
@@ -13,9 +13,11 @@ git submodule init
 git submodule update
 mkdir ~/bin
 ln -s ${ASSETS}/.vim/bundle/vimpager/vimpager ~/bin/vimpager
-ln -s ${ASSETS}/.config/qtile ~/.config/qtile
-sudo mkdir /usr/share/fonts/truetype/hack
-sudo cp ${SUBMODULES}/Hack/build/ttf/*.ttf /usr/share/fonts/truetype/hack/
+if [ "$(uname)" == "Linux" ]; then
+  ln -s ${ASSETS}/.config/qtile ~/.config/qtile
+  sudo mkdir /usr/share/fonts/truetype/hack
+  sudo cp ${SUBMODULES}/Hack/build/ttf/*.ttf /usr/share/fonts/truetype/hack/
+fi
 ln -s ${SUBMODULES}/autoenv ~/.autoenv
 ln -s ${SUBMODULES}/icdiff/icdiff ~/bin/icdiff
 ln -s ${SUBMODULES}/nvm ~/.nvm
